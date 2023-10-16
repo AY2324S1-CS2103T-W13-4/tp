a5e3519a2b0f576f5ba8ce239728c99d88f95f11
hcs1203 <solomonrajhannah@gmail.com>
2023-10-17 02:05:23 +0800

    Fixed Bugs

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_DATE_TIME = new Prefix("d/");
+    public static final Prefix PREFIX_STUDENT = new Prefix("n/");
 
 }
