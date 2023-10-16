5ba7ddf74fe7256a2027efeb66fb94696ac7bd77
hcs1203 <solomonrajhannah@gmail.com>
2023-10-17 00:32:40 +0800

    Add Appointment List

---
+++ src/main/java/seedu/address/model/appointment/AppointmentList.java
@@ -0,0 +1,121 @@
+package seedu.address.model.Appointment;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
+public class UniqueAppointmentList implements Iterable<Appointment> {
+
+    private final ObservableList<Appointment> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Appointment> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+
+    public boolean contains(Appointment toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::isSameAppointment);
+    }
+
+
+    public void add(appointment toAdd) {
+        requireNonNull(toAdd);
+        /*
+        if (contains(toAdd)) {
+            throw new DuplicateAppointmentException();
+        }
+        */
+        internalList.add(toAdd);
+    }
+
+    public void setAppointment(Appointment target, Appointment editedAppointment) {
+        requireAllNonNull(target, editedAppointment);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new AppointmentNotFoundException();
+        }
+
+        if (!target.isSameAppointment(editedAppointment) && contains(editedAppointment)) {
+            throw new DuplicateAppointmentException();
+        }
+
+        internalList.set(index, editedAppointment);
+    }
+
+    public void remove(Appointment toRemove) {
+        requireNonNull(toRemove);
+        /*
+        if (!internalList.remove(toRemove)) {
+            throw new AppointmentNotFoundException();
+        }
+        */
+    }
+
+    public void setAppointments(UniqueAppointmentList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    public void setAppointments(List<Appointment> appointments) {
+        requireAllNonNull(appointments);
+        /*
+        if (!appointmentsAreUnique(appointments)) {
+            throw new DuplicateAppointmentException();
+        }
+         */
+
+        internalList.setAll(appointments);
+
+    }
+
+    public ObservableList<Appointment> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Appointment> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof UniqueAppointmentList)) {
+            return false;
+        }
+
+        UniqueAppointmentList otherUniqueAppointmentList = (UniqueAppointmentList) other;
+        return internalList.equals(otherUniqueAppointmentList.internalList);
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return internalList.toString();
+    }
+
+    private boolean appointmentsAreUnique(List<Appointment> appointments) {
+        for (int i = 0; i < appointments.size() - 1; i++) {
+            for (int j = i + 1; j < appointments.size(); j++) {
+                if (appointments.get(i).isSameAppointment(appointments.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
