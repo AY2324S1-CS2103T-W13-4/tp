a3a12d98e5f60fe5d5444a2da780f19e5db848a2
hcs1203 <solomonrajhannah@gmail.com>
2023-10-17 02:30:21 +0800

    Fixed Bugs

--- src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
@@ -1,12 +1,13 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT;
+
 import seedu.address.logic.commands.ScheduleCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.appointment.Appointment;
-import seedu.address.model.appointment.Description;
-import seedu.address.model.appointment.DateTime;
-import seedu.address.model.appointment.Person;
-import seedu.address.logic.parser.CliSyntax;
+import java.util.stream.Stream;
 
 /**
  * Parses input arguments and creates a new ScheduleCommand object.
@@ -20,26 +21,22 @@ public class ScheduleCommandParser implements Parser<ScheduleCommand> {
      * @throws ParseException if the user input does not conform to the expected format
      */
     public ScheduleCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, CliSyntax.PREFIX_DESCRIPTION, CliSyntax.PREFIX_DATE_TIME, CliSyntax.PREFIX_STUDENT);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_DATE_TIME, PREFIX_STUDENT);
 
-        if (!arePrefixesPresent(argMultimap, CliSyntax.PREFIX_DESCRIPTION, CliSyntax.PREFIX_DATE_TIME, CliSyntax.PREFIX_STUDENT)
-                || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(Messages.MESSAGE_INVALID_COMMAND_FORMAT);
+        if (!arePrefixesPresent(argMultimap, PREFIX_DATE_TIME, PREFIX_STUDENT) || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ScheduleCommand.MESSAGE_USAGE));
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(CliSyntax.PREFIX_DESCRIPTION, CliSyntax.PREFIX_DATE_TIME, CliSyntax.PREFIX_STUDENT);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_DATE_TIME, PREFIX_STUDENT);
 
+        // Parse the appointment details from the input
         try {
-            String description = argMultimap.getValue(CliSyntax.PREFIX_DESCRIPTION).orElse(""); // Use an empty string if not provided
-            String dateTime = argMultimap.getValue(CliSyntax.PREFIX_DATE_TIME).get();
-            String student = argMultimap.getValue(CliSyntax.PREFIX_STUDENT).get();
-
+            String dateTime = argMultimap.getValue(PREFIX_DATE_TIME).get();
+            String student = argMultimap.getValue(PREFIX_STUDENT).get();
             // Additional parsing and validation logic can be added here as needed
-            Description descriptionObj = new Description(description);
-            DateTime dateTimeObj = new DateTime(dateTime);
-            Person person = new Person(student);
 
-            Appointment appointment = new Appointment(dateTimeObj, person, descriptionObj);
+            Appointment appointment = new Appointment(dateTime, student);
 
             return new ScheduleCommand(appointment);
         } catch (Exception e) {
